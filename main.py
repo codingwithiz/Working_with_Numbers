1a641d3245cb184b72ed9a3d030e44c8b7eb6b92
Ing Zhen <111691855+codingwithiz@users.noreply.github.com>
2022-08-22 12:09:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+my_num = 5
+
+print(str(my_num) + " is my favourite number")
+print(abs(my_num))
+print(pow(my_num , 5))
+print(max(4 , 6))
+print(min(4, 6))
+print(round(3.2))
+
+#start from here requires the math library
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
